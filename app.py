4f1874b36063de5cdb1d6f66c99aac8c55e1e1f4
annaandraszek <annareach2@gmail.com>
2019-05-15 15:04:12 +1000

    Added created files in preparation for making a Flask app. Routing works.

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, redirect, url_for, render_template
+app = Flask(__name__)
+from main import user_input_loop
+
+@app.route('/result/<variable>')
+def result(variable):
+    return 'Predicting %s' % variable
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        inpt = request.form['var']
+        #return render_template('predict.html')
+        return redirect(url_for('result', variable=inpt))
+    else:
+        #inpt = request.args.get('var')
+        return render_template('predict.html')
+        #return redirect(url_for('result', variable=inpt))
+
+@app.route('/')
+def home():
+    return 'Hi! Go to /predict to enter a scientific variable'
+
+if __name__ == '__main__':
+    app.run(debug=True)
