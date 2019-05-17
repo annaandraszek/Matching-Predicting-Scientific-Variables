56448504728e167ab2e6dfabe78e1e5caf1ba8e3
annaandraszek <annareach2@gmail.com>
2019-05-17 12:03:18 +1000

    Improved app predictions to display as list

--- app.py
+++ app.py
@@ -2,30 +2,34 @@ from flask import Flask, request, redirect, url_for, render_template
 app = Flask(__name__)
 from main import app_user_input, app_load_models
 
-
-bm, pm, um = app_load_models()
+app_load_models()
 
 
 @app.route('/result/<variable>')
 def result(variable):
     return 'Predicting %s' % variable
 
+@app.route('/')
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
         inpt = request.form['var']
-        #return render_template('predict.html')
+        #return render_template('indexindex.html')
         #return redirect(url_for('result', variable=inpt))
-        result = str(app_user_input(str(inpt), bm, pm, um))
-        return render_template('predict.html', prediction=result)
+        result = app_user_input(str(inpt))
+        print(result, len(result))
+        if isinstance(result, str):
+            return render_template('index.html', no_prediction=result)
+        if len(result) <= 1:
+            result = [result]
+        return render_template('index.html', prediction=result)
     else:
         #inpt = request.args.get('var')
-        return render_template('predict.html')
+        return render_template('index.html')
         #return redirect(url_for('result', variable=inpt))
 
-@app.route('/')
-def home():
-    return 'Hi! Go to /predict to enter a scientific variable'
+#def home():
+#    return 'Hi! Go to /predict to enter a scientific variable'
 
 
 if __name__ == '__main__':
