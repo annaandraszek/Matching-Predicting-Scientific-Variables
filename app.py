48f04e6f7d9e5e8f35db014b2c7fd54df80acb78
annaandraszek <annareach2@gmail.com>
2020-06-21 22:36:28 +1000

    App.py now able to run with docker

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from flask import Flask, request, redirect, url_for, render_template
 app = Flask(__name__)
 from main import app_user_input, app_load_models
@@ -80,4 +81,4 @@ def submit():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
